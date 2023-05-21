62455a695c8a7cd583f3a97bd438616c5d8a1f69
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-05-21 00:37:06 -0500

    agrege la extension Seguridad que en el cambio anterior no lo hice jajaja

--- Concesionario_EnJava/src/main/java/view/LoginController.java
+++ Concesionario_EnJava/src/main/java/view/LoginController.java
@@ -1,26 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Mr. Nicolas
- */
-public class LoginController implements Initializable {
+public class LoginController {
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    void initialize(URL url,ResourceBundle rb){
+        //TODO
+    }
+
+    @FXML
+    private PasswordField BloqueContraseña;
+
+    @FXML
+    private TextField BloqueUsuario;
+
+    @FXML
+    private Button BotonIngrese;
+
+    @FXML
+    private Button BotonRecuperar;
+
+    @FXML
+    private Rectangle FondoRojo;
+
+    @FXML
+    private ImageView ImagenLoco;
+
+    @FXML
+    private ImageView Logo;
+
+    @FXML
+    private Pane PanelBlanco;
+
+    @FXML
+    private Separator Separador;
+
+    @FXML
+    private Label TextContraseña;
+
+    @FXML
+    private Label TextoUsuario;
+
+
+
 }
