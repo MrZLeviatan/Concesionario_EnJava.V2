6beacf559419937120b8370f8b176b9c39f4b5db
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-05-21 18:28:39 -0500

    load

--- Concesionario_EnJava/src/main/java/module-info.java
+++ Concesionario_EnJava/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module view {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires javax.jms.api;
+    requires mail;
 
     opens view to javafx.fxml;
     exports view;
